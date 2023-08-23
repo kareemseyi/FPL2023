99f7524190b5a141f13342e9d0a4689dcb082436
Oluwaseyi Kareem <kareemseyi@gmail.com>
2023-08-23 11:58:12 -0600

    refactored all scripts, added notes and readme

--- scripts/FPLhistorical.py
+++ scripts/FPLhistorical.py
@@ -1,51 +1,47 @@
-import csv
-
 import sys
-import asyncio
-import requests
-# from prettytable import PrettyTable
-#
-# players_table_2021 = PrettyTable()
-# players_table_2021.field_names = ["Player_Name", "Price (Pounds)", "Goals", "Assists", "Direct Goal Contribution",
-#                                   "Total Points", "ROI", "Position", "Team"]
-# star_players = []
-#
-# with open('../raw_player_data/cleaned_players_2022_2023.csv') as f:
-#     print(f.name)
-#     reader = csv.reader(f)
-#     headers = next(reader)
-#     print(headers)
-#     for row in reader:
-#         player_fullname = row[0] + " " + row[1]
-#         player_name = row[1]
-#         goals = int(row[2])
-#         assists = int(row[3])
-#         goal_contributions = goals + assists
-#         price = int(row[-2]) / 10
-#         points = int(row[4])
-#         roi = f"{points / price:.4}"
-#         position = row[-1]
-#         players_table_2021.add_row([player_name, f"£{price}", goals, assists, goal_contributions,
-#                            points, float(roi), position, None])
-#         if goal_contributions >= 17 or (
-#             position == 'DEF' and goal_contributions > 5
-#         ):
-#             star_players.append(player_name)
-#     players_table_2021.reversesort = True
-#     print(players_table_2021.get_string(sortby='Total Points'))
-#     print(star_players)
-
 import os
 import pandas as pd
 import csv
+from prettytable import PrettyTable
 
-directory_str = 'raw_player_data'
+players_table_2021 = PrettyTable()
+players_table_2021.field_names = ["Player_Name", "Price (Pounds)", "Goals", "Assists", "Direct Goal Contribution",
+                                  "Total Points", "ROI", "Position", "Team"]
+star_players = []
+
+with open('../raw_player_data/cleaned_players_2022_2023.csv') as f:
+    print(f.name)
+    reader = csv.reader(f)
+    headers = next(reader)
+    print(headers)
+    for row in reader:
+        player_fullname = row[0] + " " + row[1]
+        player_name = row[1]
+        goals = int(row[2])
+        assists = int(row[3])
+        goal_contributions = goals + assists
+        price = int(row[-2]) / 10
+        points = int(row[4])
+        roi = f"{points / price:.4}"
+        position = row[-1]
+        players_table_2021.add_row([player_name, f"£{price}", goals, assists, goal_contributions,
+                           points, float(roi), position, None])
+        if goal_contributions >= 17 or (
+            position == 'DEF' and goal_contributions > 5
+        ):
+            star_players.append(player_name)
+    players_table_2021.reversesort = True
+    print(players_table_2021.get_string(sortby='Total Points'))
+    # print(star_players)
+
+
+directory_str = '../raw_player_data'
 directory = os.fsencode(directory_str)
 player_list = []
 
 
 def generate_historical():
-    for file in os.listdir(directory):
+    for file in os.listdir(directory_str):
         filename = os.fsdecode(file)
 
         if filename.endswith(".csv"):
@@ -53,7 +49,7 @@ def generate_historical():
             filename = os.path.join(directory_str, filename)
             df = pd.read_csv(filename)
             avg_points = df['total_points'].mean()
-            print(avg_points)
+            # print(avg_points)
             with open(filename) as f:
                 # print(f.name)
                 reader = csv.reader(f)
