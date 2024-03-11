ef3c212327bb84baafe951df9ca2bafc64abb7fe
Oluwaseyi Kareem <seyi@Oluwaseyis-MacBook-Pro-2.local>
2024-03-11 01:09:34 -0600

    Added stubs for models

--- historical/FPLhistorical.py
+++ historical/FPLhistorical.py
@@ -94,7 +94,7 @@ try:
     player_dict = generate_historical()
     keys = player_dict[0].keys()
 
-    with open('../Ballers.csv', 'w', newline='') as output_file:
+    with open('../scripts/Ballers.csv', 'w', newline='') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(player_dict)
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,8 @@
 import requests
-resp = requests.get('https://fantasy.premierleague.com/api/fixtures/')
-resp2 =  resp.json()
+resp = requests.get('https://fantasy.premierleague.com/api/fixtures/?events={26}')
+resp2 = resp.json()
 
-print(resp2[0])
+# print(len(resp2))
+
+game_weeks = [x for x in resp2 if x['finished'] is True and x['event'] == 3]
+print(game_weeks[0])
