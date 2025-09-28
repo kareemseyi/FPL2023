edd9fdd4062fbebc1051db269eaf9f726d4f8f5b
datareemz <kareemseyi@gmail.com>
2025-09-28 15:26:28 -0600

    last refactor

--- historical/trainingData.py
+++ historical/trainingData.py
@@ -1,15 +1,17 @@
 import pandas as pd
+import historical
 
-season = "24_25"  # HardCoded for now
-from historical import historical
 
-csv_file_path = f"../historical/_summary/FPL_data_{season}.csv"
-csv_file_path_dest = f"../datastore/training/FPL_data_{season}.csv"
+season = "24_25"
+csv_file_path = f"historical/_summary/FPL_data_{season}.csv"
+csv_file_path_dest = f"datastore/training/FPL_data_{season}.csv"
 
 form_dict, r = historical.getFormDict(season=season)
 print(form_dict)
 FDR = historical.get_FDR(form_dict, season=season)
 print(FDR)
+
+
 # Assumed promoted will get relegated at the start of the season
 FDR["Burnley"] = FDR["Ipswich"]
 FDR["Sunderland"] = FDR["Leicester"]
